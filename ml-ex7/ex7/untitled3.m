244cbacca3f716cca88744d21a038e077352bf87
hijerusalem <hchtgh315@gmail.com>
2017-02-11 12:01:33 +0800

    first

---
+++ ml-ex7/ex7/untitled3.m
@@ -0,0 +1,8 @@
+load('ex7data2.mat');
+K = 3; % 3 Centroids
+initial_centroids = [3 3; 6 2; 8 5];
+
+% Find the closest centroids for the examples using the
+% initial_centroids
+idx = findClosestCentroids(X, initial_centroids);
+centroids = computeCentroids(X, idx, K);
